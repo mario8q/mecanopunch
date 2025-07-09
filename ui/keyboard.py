3b33072a7f9d1ece124b138677ab7f6687c381d0
Mario Santacruz <mariofernandosantacruzp@gmail.com>
2025-07-08 19:14:26 -0500

    Corrupted BackSpaceKeyboard initialized

--- ui/keyboard.py
+++ ui/keyboard.py
@@ -52,7 +52,7 @@ class VirtualKeyboard:
             txt_padding=(5, 10)
         )
 
-        self.last_keysym_pressed = None
+        self.active_keys: set[str] = set()
 
         self.letter_key_size: tuple = (self.key_size, self.key_size)
 
@@ -83,7 +83,9 @@ class VirtualKeyboard:
         """
         Callback func para key press events 
         """
-        self.last_keysym_pressed = event.keysym
+        keysym: str = event.keysym
+        self.active_keys.add(keysym)
+
         key: Optional[kl.Key] = self.keyboard.get_key(event)
 
         if key:
@@ -94,7 +96,14 @@ class VirtualKeyboard:
         """
         Callback func para key release events
         """
-        keysym = self.last_keysym_pressed if event.keysym in ["Shift_L", "Shift_R"] else event.keysym
+        keysym: str = event.keysym
+        if keysym not in self.active_keys and "Shift" in keysym:
+            if "Shift_L" in self.active_keys:
+                keysym = "Shift_L"
+            elif "Shift_R" in self.active_keys:
+                keysym = "Shift_R"
+
+        self.active_keys.discard(keysym)
 
         if keysym == "Shift_R":
             key: Optional[kl.Key] = kl.Key.RIGHT_SHIFT
@@ -118,7 +127,7 @@ class VirtualKeyboardCorrupted(VirtualKeyboard):
     """
     Clase para crear el teclado virtual 'corrupto' del juego
     """
-    CORRUPTED_KEYS = {
+    CORRUPTED_KEYS: Dict[str, str] = {
         'q': 'p',
         'p': 'q',
         'w': 'o',
@@ -176,7 +185,6 @@ class VirtualKeyboardCorrupted(VirtualKeyboard):
         """
         Override para crear el teclado 'corrupto' del juego
         """
-        # Estilo diferente y diseño azerty visible (solo apariencia)
         self.layout_name: LayoutName = LayoutName.QWERTY  # type: ignore[attr-defined]
         corrupted_color: str = "#350808"
         self.keyboard_info: kl.KeyboardInfo = kl.KeyboardInfo(
@@ -199,6 +207,8 @@ class VirtualKeyboardCorrupted(VirtualKeyboard):
             txt_font=tkf.Font(family='Terminal', size=18, weight='bold'),
             txt_padding=(5, 10)
         )
+
+        self.active_keys: set[str] = set()
 
         self.letter_key_size: tuple = (self.key_size, self.key_size)
 
@@ -255,7 +265,7 @@ class VirtualKeyboardCorrupted(VirtualKeyboard):
 
     def translate_corrupted_keys(self, symbol: str) -> str:
         """
-        Funcion que traduce el symbolo en teclado QWERTY a AZERTY
+        Funcion que traduce los simbolos del teclado cambiando su orden
         """
         return self.CORRUPTED_KEYS.get(symbol, symbol)
 
@@ -263,7 +273,8 @@ class VirtualKeyboardCorrupted(VirtualKeyboard):
         """
         Override para pressed keys
         """
-        self.last_keysym_pressed: str = event.keysym
+        keysym: str = event.keysym
+        self.active_keys.add(keysym)
         
         corrupted_keys_colors: List[str] = ["#40ff40", "#e12e7a", "#c900ff", "#f9fb00"]
         random_corrupted_color: str = choice(corrupted_keys_colors)
@@ -275,7 +286,7 @@ class VirtualKeyboardCorrupted(VirtualKeyboard):
             txt_padding=(5, 10)
         )
 
-        translated: str = self.translate_corrupted_keys(event.keysym)
+        translated: str = self.translate_corrupted_keys(keysym)
         matching_key: Union[Optional[kl.Key], None] = next((k for k in Key if k.value == translated), None)
             
         key: Optional[kl.Key] = self.keyboard.get_key(event)
@@ -293,7 +304,15 @@ class VirtualKeyboardCorrupted(VirtualKeyboard):
         """
         Override para released events en key
         """
-        keysym: Union[str, None] = self.last_keysym_pressed if event.keysym in ["Shift_L", "Shift_R"] else event.keysym
+        keysym: str = event.keysym
+        if keysym not in self.active_keys and "Shift" in keysym:
+            if "Shift_L" in self.active_keys:
+                keysym = "Shift_L"
+            elif "Shift_R" in self.active_keys:
+                keysym = "Shift_R"
+
+        self.active_keys.discard(keysym)
+
         translated: str = self.translate_corrupted_keys(event.keysym)
         matching_key: Union[Optional[kl.Key], None] = next((k for k in Key if k.value == translated), None)
 
@@ -322,6 +341,102 @@ class VirtualKeyboardCorrupted(VirtualKeyboard):
         print(f"Key released: {event.keysym}")
         print(f"Original: {matching_key} → Corrupto: {translated}")
 
-        
+class VirtualKeyboardCorruptedBackSpace(VirtualKeyboardCorrupted):
+    """
+    Clase para crear una variacion de teclado con la tecla backspace corrupta
+    """
+    def create_keyboard(self):
+        """
+        Override para crear el teclado corrupto con la tecla backspace unica
+        """
+        self.layout_name: LayoutName = LayoutName.QWERTY  # type: ignore[attr-defined]
+        corrupted_color: str = "#350808"
+        self.keyboard_info: kl.KeyboardInfo = kl.KeyboardInfo(
+            position=(0, 0),
+            padding=4,
+            color=corrupted_color
+        )
+        self.key_info: kl.KeyInfo = kl.KeyInfo(
+            margin=4,
+            color="#25194e",
+            txt_color="#b3b2b2",
+            txt_font=tkf.Font(family='Terminal', size=18, weight='bold'),
+            txt_padding=(5, 10)
+        )
 
+        self.released_style: kl.KeyInfo = kl.KeyInfo(
+            margin=4,
+            color="#25194e",
+            txt_color="#b3b2b2",
+            txt_font=tkf.Font(family='Terminal', size=18, weight='bold'),
+            txt_padding=(5, 10)
+        )
+
+        self.active_keys: set[str] = set()
+
+        self.letter_key_size: tuple = (self.key_size, self.key_size)
+
+        self.keyboard: klt.KeyboardLayout = klt.KeyboardLayout(
+            self.layout_name,
+            self.keyboard_info,
+            self.letter_key_size,
+            self.key_info,
+            master=self.master
+        )
+        self.bind_keyboard_events()
+        
+        self.custom_keys_style: kl.KeyInfo = kl.KeyInfo(
+            margin=4,
+            color="#a000b4",
+            txt_color="#b3b2b2",
+            txt_font=tkf.Font(family='Terminal', size=18, weight='bold'),
+            txt_padding=(5, 10)
+        )
+
+        self.keyboard.update_key(kl.Key.BACKSPACE, self.custom_keys_style)
+
+    def translate_corrupted_keys(self, symbol: str) -> str:
+        if symbol == "BackSpace":
+            return "None"  # Mapeamos a una tecla inexistente para que no reaccione
+        return symbol
+    
+    def on_key_release(self, event) -> None:
+        """
+        Override para released events en key para actualizar la tecla backspace
+        """
+        keysym: str = event.keysym
+        if keysym not in self.active_keys and "Shift" in keysym:
+            if "Shift_L" in self.active_keys:
+                keysym = "Shift_L"
+            elif "Shift_R" in self.active_keys:
+                keysym = "Shift_R"
+
+        self.active_keys.discard(keysym)
+
+        translated: str = self.translate_corrupted_keys(event.keysym)
+        matching_key: Union[Optional[kl.Key], None] = next((k for k in Key if k.value == translated), None)
+
+        if keysym == "Shift_R":
+            key: Optional[kl.Key] = kl.Key.RIGHT_SHIFT
+        elif keysym == "Shift_L":
+            key: Optional[kl.Key] = kl.Key.LEFT_SHIFT
+        else:
+            key = self.keyboard.get_key(event)
+
+        if matching_key:
+            self.keyboard.update_key(matching_key, self.released_style)
+        elif key:
+            self.keyboard.update_key(key, self.released_style)
+
+        self.custom_keys_style: kl.KeyInfo = kl.KeyInfo(
+            margin=4,
+            color="#a000b4",
+            txt_color="#b3b2b2",
+            txt_font=tkf.Font(family='Terminal', size=18, weight='bold'),
+            txt_padding=(5, 10)
+        )
+        self.keyboard.update_key(kl.Key.BACKSPACE, self.custom_keys_style)
+        
+        print(f"Key released: {event.keysym}")
+        print(f"Original: {matching_key} → Corrupto: {translated}")
     
